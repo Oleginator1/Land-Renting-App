523e923a00b9401e4f3ef17b6b1a6c64c13d9883
Nastas Oleg <olegnastas9@gmail.com>
2026-04-29 21:07:22 +0300

    Implemented a connection to the database

---
+++ Config/AppSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandRentManagementApp.Config
+{
+    public static class AppSettings
+    {
+        public static string ConnectionString { get; private set; } = "Server=DESKTOP-T924N09;Database=LandRentDB;Trusted_Connection=True;TrustServerCertificate=True";
+
+        public static void SetConnectionString(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+    }
+}
