2da333b143d4b43ee4255af645dfed07599aff9c
Nastas Oleg <olegnastas9@gmail.com>
2026-04-30 20:35:32 +0300

    Created the design of the menu form

---
+++ Forms/MainForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace LandRentManagementApp.Forms
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem menuItemDate;
+        private ToolStripMenuItem menuFermieri;
+        private ToolStripMenuItem menuTerenuri;
+        private ToolStripMenuItem menuContracte;
+        private ToolStripMenuItem menuItemRapoarte;
+        private ToolStripMenuItem menuRaport;
+        private ToolStripMenuItem menuItemAplicatie;
+        private ToolStripMenuItem menuIesire;
+        private Panel panelLeft;
+        private Panel panelContent;
+        private StatusStrip statusStrip;
+        private ToolStripStatusLabel statusLabel;
+        private Label lblTitlu;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && components != null) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+
+            menuStrip = new MenuStrip();
+            menuItemDate = new ToolStripMenuItem();
+            menuFermieri = new ToolStripMenuItem();
+            menuTerenuri = new ToolStripMenuItem();
+            menuContracte = new ToolStripMenuItem();
+            menuItemRapoarte = new ToolStripMenuItem();
+            menuRaport = new ToolStripMenuItem();
+            menuItemAplicatie = new ToolStripMenuItem();
+            menuIesire = new ToolStripMenuItem();
+            panelLeft = new Panel();
+            panelContent = new Panel();
+            statusStrip = new StatusStrip();
+            statusLabel = new ToolStripStatusLabel();
+            lblTitlu = new Label();
+
+
+            menuStrip.BackColor = Color.FromArgb(34, 85, 34);
+            menuStrip.ForeColor = Color.White;
+            menuStrip.Font = new Font("Segoe UI", 10f, FontStyle.Regular);
+            menuStrip.Items.AddRange(new ToolStripItem[]
+                { menuItemDate, menuItemRapoarte, menuItemAplicatie });
+
+            menuItemDate.Text = "📋 Date";
+            menuItemDate.ForeColor = Color.White;
+            menuItemDate.DropDownItems.AddRange(new ToolStripItem[]
+                { menuFermieri, menuTerenuri, menuContracte });
+
+            menuFermieri.Text = "👨‍🌾 Fermieri";
+            menuTerenuri.Text = "🌾 Terenuri";
+            menuContracte.Text = "📄 Contracte de Arendă";
+            menuFermieri.Click += menuFermieri_Click;
+            menuTerenuri.Click += menuTerenuri_Click;
+            menuContracte.Click += menuContracte_Click;
+
+            menuItemRapoarte.Text = "📊 Rapoarte";
+            menuItemRapoarte.ForeColor = Color.White;
+            menuItemRapoarte.DropDownItems.Add(menuRaport);
+            menuRaport.Text = "📈 Raport Financiar";
+            menuRaport.Click += menuRaport_Click;
+
+            menuItemAplicatie.Text = "⚙ Aplicație";
+            menuItemAplicatie.ForeColor = Color.White;
+            menuItemAplicatie.DropDownItems.Add(menuIesire);
+            menuIesire.Text = "🚪 Ieșire";
+            menuIesire.Click += menuIesire_Click;
+
+
+            panelLeft.BackColor = Color.FromArgb(45, 100, 45);
+            panelLeft.Dock = DockStyle.Left;
+            panelLeft.Width = 200;
+
+            lblTitlu.Text = "🌾 Arendă\nManagement";
+            lblTitlu.ForeColor = Color.White;
+            lblTitlu.Font = new Font("Segoe UI", 13f, FontStyle.Bold);
+            lblTitlu.TextAlign = ContentAlignment.MiddleCenter;
+            lblTitlu.Dock = DockStyle.Top;
+            lblTitlu.Height = 80;
+            lblTitlu.Padding = new Padding(0, 10, 0, 0);
+            panelLeft.Controls.Add(lblTitlu);
+
+         
+            AddNavButton(panelLeft, "👨‍🌾 Fermieri", menuFermieri_Click, 80);
+            AddNavButton(panelLeft, "🌾 Terenuri", menuTerenuri_Click, 130);
+            AddNavButton(panelLeft, "📄 Contracte", menuContracte_Click, 180);
+            AddNavButton(panelLeft, "📊 Raport", menuRaport_Click, 230);
+
+     
+            panelContent.Dock = DockStyle.Fill;
+            panelContent.BackColor = Color.WhiteSmoke;
+            panelContent.Padding = new Padding(5);
+
+            statusStrip.Items.Add(statusLabel);
+            statusLabel.Text = "Se inițializează...";
+
+
+            Text = "Sistem de Evidență Arendă";
+            Size = new Size(1100, 680);
+            StartPosition = FormStartPosition.CenterScreen;
+            MinimumSize = new Size(900, 600);
+            MainMenuStrip = menuStrip;
+
+            Controls.Add(panelContent);
+            Controls.Add(panelLeft);
+            Controls.Add(menuStrip);
+            Controls.Add(statusStrip);
+        }
+
+        private void AddNavButton(Panel parent, string text,
+            EventHandler handler, int top)
+        {
+            var btn = new Button
+            {
+                Text = text,
+                Top = top,
+                Left = 10,
+                Width = 180,
+                Height = 40,
+                FlatStyle = FlatStyle.Flat,
+                ForeColor = Color.White,
+                BackColor = Color.FromArgb(55, 120, 55),
+                Font = new Font("Segoe UI", 10f),
+                Cursor = Cursors.Hand,
+                TextAlign = ContentAlignment.MiddleLeft,
+                Padding = new Padding(10, 0, 0, 0)
+            };
+            btn.FlatAppearance.BorderColor = Color.FromArgb(80, 150, 80);
+            btn.Click += handler;
+            parent.Controls.Add(btn);
+        }
+    }
+}
---
+++ Forms/MainForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LandRentManagementApp.Data;
+
+namespace LandRentManagementApp.Forms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            TestConnection();
+            
+        }
+
+        private void TestConnection()
+        {
+            if (DatabaseHelper.TestConnection())
+            {
+                statusLabel.Text = "✔ Conectat la baza de date";
+                statusLabel.ForeColor = Color.Green;
+            }
+            else
+            {
+                statusLabel.Text = "✘ Eroare conexiune baza de date";
+                statusLabel.ForeColor = Color.Red;
+                MessageBox.Show(
+                    "Nu s-a putut conecta la baza de date!\n" +
+                    "Verificați că SQL Server este pornit și că stringul de conexiune este corect.",
+                    "Eroare conexiune",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+        
+        private void VerificaConexiune()
+        {
+            if (DatabaseHelper.TestConnection())
+            {
+                statusLabel.Text = "✔ Conectat la baza de date";
+                statusLabel.ForeColor = Color.Green;
+            }
+            else
+            {
+                statusLabel.Text = "✘ Eroare conexiune baza de date";
+                statusLabel.ForeColor = Color.Red;
+                MessageBox.Show(
+                    "Nu s-a putut conecta la baza de date!\n" +
+                    "Verificați că SQL Server este pornit și că stringul de conexiune este corect.",
+                    "Eroare conexiune",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void AfiseazaFormular(Form form)
+        {
+            panelContent.Controls.Clear();
+            form.TopLevel = false;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.Dock = DockStyle.Fill;
+            panelContent.Controls.Add(form);
+            form.Show();
+        }
+
+
+        private void menuFermieri_Click(object sender, EventArgs e)
+            => AfiseazaFormular(new FermierListForm());
+
+        private void menuTerenuri_Click(object sender, EventArgs e)
+            => AfiseazaFormular(new LandListForm());
+
+        private void menuContracte_Click(object sender, EventArgs e)
+            => AfiseazaFormular(new ContractListForm());
+
+        private void menuRaport_Click(object sender, EventArgs e)
+            => new ReportForm().ShowDialog(this);
+
+        private void menuIesire_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Doriți să închideți aplicația?",
+                "Confirmare", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question) == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
