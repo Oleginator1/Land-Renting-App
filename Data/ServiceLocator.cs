88b21e5b9058df77f7d2d31fe30306ed9a87979a
Nastas Oleg <olegnastas9@gmail.com>
2026-05-06 18:43:53 +0300

    Created service locator #11

---
+++ Data/ServiceLocator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandRentManagementApp.Data
+{
+    public static class ServiceLocator
+    {
+        private static IFarmerRepository? _farmerRepo;
+        private static ILandRepository? _landRepo;
+        private static IContractRepository? _contractRepo;
+
+        public static IFarmerRepository FarmerRepo =>
+            _farmerRepo ??= new FarmerRepository();
+
+        public static ILandRepository TerenRepo =>
+            _landRepo ??= new LandRepository();
+
+        public static IContractRepository ContractRepo =>
+            _contractRepo ??= new ContractRepository();
+
+        
+        public static void Register(IFarmerRepository repo) => _farmerRepo = repo;
+        public static void Register(ILandRepository repo) => _landRepo = repo;
+        public static void Register(IContractRepository repo) => _contractRepo = repo;
+    }
+}
